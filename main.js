df57d3dbe89b6942835213fd2521cfe798d42bb8
JosePabloMonteroC <pepemonterok2002@gmail.com>
2024-01-07 15:09:41 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('moving-button').addEventListener('mouseover', function() {
+    var x = Math.random() * window.innerWidth;
+    var y = Math.random() * window.innerHeight;
+    this.style.left = x + 'px';
+    this.style.top = y + 'px';
+});
+document.getElementById('static-button').addEventListener('mouseover', function() {
+    document.querySelector('.title').innerHTML = 'Yo tambien, te amo :)';
+});
