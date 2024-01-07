61367123581be7fd7cdee51d0081e8e4965caef7
jpmontero75 <pepemonterok2002@gmail.com>
2024-01-07 15:18:26 -0600

    bug

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-document.getElementById('moving-button').addEventListener('mouseover', function() {
+var movingButton = document.getElementById('moving-button');
+movingButton.addEventListener('mouseover', moveButton);
+movingButton.addEventListener('touchstart', moveButton);
+
+function moveButton() {
     var x = Math.random() * window.innerWidth;
     var y = Math.random() * window.innerHeight;
     this.style.left = x + 'px';
     this.style.top = y + 'px';
-});
+}
+
 document.getElementById('static-button').addEventListener('mouseover', function() {
     document.querySelector('.title').innerHTML = 'Yo tambien, te amo :)';
 });
